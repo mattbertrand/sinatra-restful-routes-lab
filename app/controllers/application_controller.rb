03a652a6527860d8fdc4d1048c2af66ce2562c93
github username <github email address>
2020-12-24 10:53:26 -0800

    commit tree 0ef00785dee969947cb81779ad8ee564c2d6d25e

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
+require './config/environment'
+
 class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
@@ -6,4 +8,48 @@ class ApplicationController < Sinatra::Base
 
   # code actions here!
 
+  get '/recipes/new' do
+    erb :new
+  end
+  
+  get '/recipes' do
+    @recipes = Recipe.all
+    erb :index
+  end
+
+  get '/recipes/:id' do
+    find_by_id
+    erb :show
+  end
+
+  get '/recipes/:id/edit' do
+    find_by_id
+    erb :edit
+  end
+
+  patch '/recipes/:id' do
+    find_by_id
+    @recipe.name = params[:name]
+    @recipe.ingredients = params[:ingredients]
+    @recipe.cook_time = params[:cook_time]
+    @recipe.save
+    redirect to "/recipes/#{@recipe.id}"
+  end
+
+  post '/recipes' do
+    @recipe = Recipe.create(params)
+    redirect to "/recipes/#{@recipe.id}"
+  end
+
+  delete '/recipes/:id' do
+    find_by_id
+    @recipe.delete
+    redirect to '/recipes'
+  end
+
+  private
+
+  def find_by_id
+    @recipe = Recipe.find_by_id(params[:id])
+  end
 end
